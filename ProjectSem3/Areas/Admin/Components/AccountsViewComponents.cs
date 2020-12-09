a1dec7cdd9b7b7ae49e57a82257f75182a8183a4
HuyGin <57339510+huygin1@users.noreply.github.com>
2020-12-09 13:40:27 +0700

    Add project files.

---
+++ ProjectSem3/Areas/Admin/Components/AccountsViewComponents.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectSem3.Areas.Admin.Models;
+
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+namespace ProjectSem3.Areas.Admin.Components
+{
+    [ViewComponent(Name = "Accounts")]
+    public class CardViewComponent : ViewComponent
+    {
+        public string url = "http://localhost:61505/api/Accounts";
+        private HttpClient httpClient = new HttpClient();
+       
+       
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string _ID = HttpContext.Session.GetInt32("_ID").ToString();
+            var model = JsonConvert.DeserializeObject<IEnumerable<Accounts>>(httpClient.GetStringAsync(url).Result);
+           
+            var acc = from s in model
+                      select s;
+            acc = acc.Where(a => a.AccountID.ToString().Equals(_ID));
+            return View(acc);
+        }
+
+    }
+}
---
+++ ProjectSem3/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectSem3.Areas.Admin.Models;
+using System.Net.Http;
+namespace ProjectSem3.Areas.Admin.Controllers
+{ 
+    [Area("Admin")]
+    [Route("Admin/Home")]
+    public class HomeController : Controller
+    {
+        public string url = "http://localhost:61505/api/";
+        private HttpClient httpClient = new HttpClient();
+        public bool CheckLogin()
+        {
+            string Role = HttpContext.Session.GetString("_Role");
+            if (string.IsNullOrEmpty(Role))
+            {
+                return false;
+
+            }
+            else
+            {
+                string _Role = Role.ToUpper();
+                if (_Role.Equals("ADMIN"))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+        [Route("index")]
+        public IActionResult Index()
+        {
+            
+            if (!CheckLogin())return RedirectToAction("Login");
+            return View();
+
+        }
+        [Route("Profile")]
+        public IActionResult Profile()
+        {
+            if (!CheckLogin()) return RedirectToAction("Login");
+            return View();
+        }
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [Route("Login")]
+        [HttpPost]
+        public IActionResult Login(string UserName,string PassWord)
+        {
+            
+            var model = JsonConvert.DeserializeObject<IEnumerable<Accounts>>(httpClient.GetStringAsync(url+ "Accounts/").Result);
+            Accounts accounts = model.Where(e => e.AccountName.Equals(UserName)).FirstOrDefault();
+            if (accounts != null)
+            {
+                if (accounts.Password.Equals(PassWord))
+                {
+                    HttpContext.Session.SetString("_UserName", UserName);
+                    HttpContext.Session.SetString("_Image", "avatar.png");
+                    HttpContext.Session.SetString("_Role", accounts.Role);
+                    HttpContext.Session.SetInt32("_ID",accounts.AccountID);
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    ViewBag.Msg = "Password wrong!!!";
+                }
+                
+               
+            
+            }
+            return View();
+        }
+    }
+}
